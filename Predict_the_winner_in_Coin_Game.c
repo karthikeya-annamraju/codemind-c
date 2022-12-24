b013f3b6f5d05f6d27ec88cee2253c6ffee952b0
Karthikeya Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2022-12-24 18:24:12 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d%d",&n,&m);     if(n%2==0 or m%2==0) {         printf("Player 1");     }     else{         printf("Player 2");     } }

---
+++ Predict_the_winner_in_Coin_Game.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d%d",&n,&m);
+    if(n%2==0 or m%2==0) {
+        printf("Player 1");
+    }
+    else{
+        printf("Player 2");
+    }
+}
