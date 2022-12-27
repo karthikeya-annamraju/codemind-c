9832bda1f0edb7a3745d481f08bd44497d7910eb
Karthikeya Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2022-12-27 12:03:06 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for (j=1;j<=n;j++) {         for (i=1;i<=j;i++) {             printf("%d",i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -4,8 +4,8 @@ int main()
     int n,i,j;
     scanf("%d",&n);
     for (j=1;j<=n;j++) {
-        for (i=n;i>=1;i--) {
-            printf("%d ",i);
+        for (i=1;i<=j;i++) {
+            printf("%d",i);
         }
         printf("
 ");
