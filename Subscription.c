7beda731b7ab7529faba784555a7ca4634177e4f
Karthikeya Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2022-12-28 22:52:40 +0530

    #include<stdio.h> int main() {     int n,x,i;     scanf("%d",&n);     for (i=1;i<=n;i++) {         scanf("%d",&x);         if (x>30) printf("YES ");         else printf("NO ");     } }

---
+++ Subscription.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,x,i;
+    scanf("%d",&n);
+    for (i=1;i<=n;i++) {
+        scanf("%d",&x);
+        if (x>30) printf("YES
+");
+        else printf("NO
+");
+    }
+}
