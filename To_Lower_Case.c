6aa32348a454b56af04d4b667539ac1910dd43cd
Karthikeya Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2023-01-24 17:45:25 +0530

    #include <stdio.h> #include <string.h> #include <ctype.h> int main() { char s[200]; scanf("%[^ ]s",s); for(int i=0;i<strlen(s);i++) { 	s[i]=tolower(s[i]); } printf("%s",s); return 0; }

---
+++ To_Lower_Case.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main() 
+{
+char s[200];
+scanf("%[^
+]s",s);
+for(int i=0;i<strlen(s);i++)
+{
+	s[i]=tolower(s[i]);
+}
+printf("%s",s);
+return 0;
+}
