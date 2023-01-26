9cbbafe1c4ec0cfd711e4a89e087e5882a1163ef
Karthikeya Annamraju <86823119+karthikeya-annamraju@users.noreply.github.com>
2023-01-26 17:22:57 +0530

    #include<stdio.h> int main() {     int ar[100],i,n;     scanf("%d",&n);     for(i=0; i<n; i++)     {         scanf("%d",&ar[i]);     }
        for(i=0;i<n;i++)
        {
            if(ar[i]%2==0)
            {
                printf("%d ",ar[i]);
            }
        }
        for(i=0;i<n;i++)
        {
            if(ar[i]%2!=0)
            {
                printf("%d ",ar[i]);
            }
        }
    }

---
+++ even_elements_followed_by_odd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int ar[100],i,n;
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+
+    
+    for(i=0;i<n;i++)
+    {
+        if(ar[i]%2==0)
+        {
+            printf("%d ",ar[i]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(ar[i]%2!=0)
+        {
+            printf("%d ",ar[i]);
+        }
+    }
+}
